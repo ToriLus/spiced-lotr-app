9f355f284cfab290fa6129127d8aa757543a0530
Tori Luszczek <wuszczek@gmail.com>
2023-04-21 13:59:28 +0200

    did most of the challenges, the insides of each page are missing but the images work so far

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import { introduction, volumes } from "../lib/data";
+import Image from "next/image";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Lord of the Rings is a good series!</h1>
+      <p>{introduction}</p>
+      <h2>All volumes</h2>
+
+      {/* we wanna do this dynamically */}
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Image
+              src={`/../public${volume.cover}`}
+              height={150}
+              width={144}
+              alt="Cover image of the volume"
+            />
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,7 @@
+export default function TheRings() {
+  return (
+    <div>
+      <h1>book title</h1>
+    </div>
+  );
+}
